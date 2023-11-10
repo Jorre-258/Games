9a787aa3ff90e1a0b2ae09ed146f7faddb506e19
JoranQuinten01 <JoranQuinten01@gmail.com>
2023-11-10 20:35:04 +0100

    10/11/23

--- hangman.py
+++ hangman.py
@@ -1,18 +1,10 @@
 import random
 
-def rules():
-    print("Welkom bij Galgje!")
-    print("Het doel van het spel is om het woord te raden door letters te raden.")
-    print("Je hebt een beperkt aantal pogingen voordat de 'galg' compleet is.")
-    print("Als je alle letters van het woord raadt voordat de 'galg' compleet is, win je het spel.")
-    print("Als de 'galg' compleet is voordat je het woord raadt, verlies je het spel.")
-    print("Hier zijn de regels:")
-    print("1. Het spel selecteert willekeurig een woord uit een lijst.")
-    print("2. Je kunt letters raden door ze in te voeren.")
-    print("3. Als de letter in het woord voorkomt, wordt deze onthuld. Als de letter meerdere keren voorkomt, worden alle voorkomens onthuld.")
-    print("4. Als de letter niet in het woord voorkomt, verlies je een poging.")
-    print("5. Je kunt geen letters raden die je al eerder hebt geraden.")
-    print("Veel succes en veel plezier met Galgje")
+def replace_char(string, index, replacement):
+    string_list = list(string)
+    string_list[index] = replacement
+    new_string = ''.join(string_list)
+    return new_string
 
 def random_word():
     words = [
@@ -31,28 +23,35 @@ def random_word():
     random_word = random.choice(words)
     return random_word
 
-def correct_word():
-    pass
-
-def ask_word():
-    word = input()
-    return word
-
-def wrong_word():
-    pass
-
 def main():
     r_word = random_word()
-    print(r_word)
-    print('*' * len(r_word))
+    #print(r_word)
+    guess = '*' * len(r_word)
+    #print(guess)
+    tries = 10
     while True:
-        user_word = input("Welke letters zijn er in het woord?:")
-        for letter, index in enumerate(user_word):
-            if letter in r_word:
-                print(letter, index)
+        user_word = input("Welke letters zijn er in het woord?: ")
+        point = True
+        if len(user_word) != 1:
+            print("Voer slechts één letter in.")
+            continue
 
+        for i in range(len(r_word)):
+            if r_word[i] == user_word:
+                guess = guess[:i] + user_word + guess[i + 1:]
+                point = False
+        print(f'{guess}\nJe hebt nog {tries} pogingen over.\n------------------------')
 
+        if tries == 0:
+            print("Verloren")
+            break
 
+        if point:
+            tries -= 1
+
+        if '*' not in guess:
+            print("Gewonnen")
+            break
 
 
 main()
